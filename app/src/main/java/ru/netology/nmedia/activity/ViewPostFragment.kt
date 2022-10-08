289eb2c56d0ca0769b96ffdda4090388785372ef
irushenka <sindirina@mail.ru>
2022-10-08 19:12:36 +0300

    fragments

---
+++ app/src/main/java/ru/netology/nmedia/activity/ViewPostFragment.kt
@@ -0,0 +1,93 @@
+package ru.netology.nmedia.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.nmedia.R
+import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
+import ru.netology.nmedia.adapter.OnInteractionListener
+import ru.netology.nmedia.adapter.PostViewHolder
+import ru.netology.nmedia.databinding.FragmentPostBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.util.LongArg
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class ViewPostFragment : Fragment() {
+
+    companion object {
+        var Bundle.longArg: Long? by LongArg
+    }
+
+    private val viewModel: PostViewModel by viewModels(
+        ownerProducer = ::requireParentFragment
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentPostBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        val viewHolder = PostViewHolder(binding.post, object: OnInteractionListener {
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+                findNavController().navigate(R.id.action_viewPostFragment_to_editPostFragment,
+                    Bundle().apply {
+                        textArg = post.content
+                    })
+            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                    type = "text/plain"
+                }
+
+                val shareIntent =
+                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                startActivity(shareIntent)
+            }
+
+            override fun onShowDetails(post: Post) {  }
+        })
+        val id = arguments?.longArg
+        viewModel.data.observe(viewLifecycleOwner) { posts ->
+            val post = posts.find { it.id == id } ?: run {
+                findNavController().navigateUp()
+                return@observe
+            }
+            viewHolder.bind(post)
+        }
+
+
+//        arguments?.textArg
+//            ?.let(binding.edit::setText)
+//
+//        binding.ok.setOnClickListener {
+//            viewModel.changeContent(binding.edit.text.toString())
+//            viewModel.save()
+//            AndroidUtils.hideKeyboard(requireView())
+//            findNavController().navigateUp()
+//        }
+        return binding.root
+    }
+}
