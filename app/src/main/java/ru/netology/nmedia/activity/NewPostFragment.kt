1e37615c81bb7548a129a552cd2694052ccbe38d
irushenka <sindirina@mail.ru>
2022-10-09 16:35:20 +0300

    sqlite

--- app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -42,6 +43,18 @@ class NewPostFragment : Fragment() {
             AndroidUtils.hideKeyboard(requireView())
             findNavController().navigateUp()
         }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (binding.edit.text != null) {
+                    viewModel.setNotSavedText(binding.edit.text.toString())
+                }
+
+                isEnabled = false
+                activity?.onBackPressed()
+            }
+        })
+
         return binding.root
     }
 }
